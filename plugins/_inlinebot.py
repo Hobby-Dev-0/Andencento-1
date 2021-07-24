b645ee093241924d10dd70f798a529f552151f50
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-07-24 13:28:25 +0530

    Update _inlinebot.py

--- plugins/_inlinebot.py
+++ plugins/_inlinebot.py
@@ -26,7 +26,7 @@ if Config.BOT_USERNAME is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© Andencento-UserBot Help",
-                text=f"🔰 **{andencento}**\n\n📜 __No.of Plugins__ : `{len(CMD_LIST)}` \n🗂️ __Commands__ : `{len(apn)}`\n🗒️ __Page__ : 1/{veriler[0]}",
+                text=f"🔰 **{andencento}**\n\n📜 __No.of Plugins__ : `{len(CMD_LIST)}` \n🗂️ __Commands__ : `{len(apn)}`\n,
                 buttons=buttons,
                 link_preview=False,
             )
